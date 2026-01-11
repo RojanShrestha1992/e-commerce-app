733164a2117e84a5ad8de1adc6dffcb5c858b754
Rojan Shrestha <pheonixeder@gmail.com>
2026-01-11 17:50:45 +0545

    e-commerce added navbar

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import { useEffect, useState } from "react";
+import axios from "axios";
+function App() {
+  const [openDropDown, setOpenDropDown] = useState(false);
+
+  const [location, setLocation] = useState()
+  const getLocation = async () => {
+    navigator.geolocation.getCurrentPosition(async pos => {
+      const {latitude, longitude} = pos.coords
+      console.log(latitude,longitude);
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`
+      try{
+        const location = await axios.get(url)
+        // console.log(location)
+        const exactLocation = location.data.address
+        setLocation(exactLocation)
+        setOpenDropDown(false)
+      }catch (error){
+        console.log(error)
+      }
+    })
+  }
+  useEffect(()=> {
+    getLocation()
+  }, [])
+
+
+  return (
+    <BrowserRouter>
+    <Navbar location={location} openDropDown={openDropDown} setOpenDropDown={setOpenDropDown}  getLocation={getLocation}s/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { MapPin } from "lucide-react";
+import { FaCaretDown } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { CgClose } from "react-icons/cg";
+
+const Navbar = ({ location, openDropDown, setOpenDropDown, getLocation }) => {
+  const toggleDropDown = () => {
+    setOpenDropDown(!openDropDown);
+  };
+
+  return (
+    <div className="bg-white py-3 shadow-2xl ">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        {/* logo  */}
+        <div className="flex gap-7 items-center">
+          <Link to={"/"}>
+            <h1 className="font-extrabold text-3xl">
+              <span className="text-blue-500 font-serif">B</span>azzar
+            </h1>
+          </Link>
+          <div className="flex gap-1 cursor-pointer text-gray-700 items-center ">
+            <MapPin className="text-blue-500" />
+            <span className="font-semibold">
+              {location ? (
+                <div className="-space-y-2">
+                  <p>{location.county}</p>
+                  <p>{location.state}</p>
+                </div>
+              ) : (
+                "Add Address"
+              )}
+            </span>
+            <FaCaretDown onClick={toggleDropDown} />
+          </div>
+          {openDropDown ? (
+            <div className="w-62.5 h-max shadow-2xl z-50 bg-white fixed top-16 left-60 border-2 p-5 border-gray-100 rounded-md">
+              <h1 className="font-semibold mb-4 text-xl flex justify-between">
+                Change Location{" "}
+                <span onClick={toggleDropDown} className="cursor-pointer">
+                  <CgClose />
+                </span>
+              </h1>
+              <button
+                onClick={getLocation}
+                className="bg-red-500 text-white px-3 py-1 rounded-md cursor-pointer hover:bg-red-400"
+              >
+                Detect My Location
+              </button>
+            </div>
+          ) : null}
+        </div>
+
+        {/* menu  */}
+        <nav className="flex gap-7 items-center">
+          <ul className="flex gap-7 items-center text-xl font-semibold">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all duration-200 border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to={"products"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all  duration-200 border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Products</li>
+            </NavLink>
+            <NavLink
+              to={"about"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all  duration-200 border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to={"contact"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all  duration-200 border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+          <Link to={"/cart"} className="relative">
+            <IoCartOutline className="h-7 w-7" />
+            <span className="bg-red-500 -top-3 -right-3 px-2 rounded-full absolute">
+              0
+            </span>
+          </Link>
+
+          <div>
+            <SignedOut>
+              <SignInButton className="bg-red-500 text-white px-3 py-1 rounded-md cursor-pointer" />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
